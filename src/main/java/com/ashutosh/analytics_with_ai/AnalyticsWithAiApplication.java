ef5e31bad66fd76519aea486e1f2ff9796cd976d
Ashu9507 <ashukrdos@gmail.com>
2026-05-23 16:59:33 +0530

    first commit

---
+++ src/main/java/com/ashutosh/analytics_with_ai/AnalyticsWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.ashutosh.analytics_with_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsWithAiApplication.class, args);
+	}
+
+}
